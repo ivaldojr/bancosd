e34d333859a5bce0b0580db43023f5dd9030f11d
ivaldojr <ivaldo.martins.jr@hotmail.com>
2015-05-28 17:24:24 -0300

    Migrando projeto que iniciou.

---
+++ bancosd/src/ServidorMulticast/ServidorMulticast.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ServidorMulticast;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ivaldojunior
+ */
+public class ServidorMulticast {
+    public static void main(String [] args){
+        try {
+            int port = 2525;
+            String ip = "225.4.5.6";
+            
+            MulticastSocket s  = new MulticastSocket(port);
+            s.joinGroup(InetAddress.getByName(ip));
+            
+            byte buf[] = new byte[1024];
+            
+            DatagramPacket pacote = new DatagramPacket(buf, buf.length);
+            s.receive(pacote);
+            
+            System.out.println("Dados recebido de:"+ pacote.getAddress().toString()+
+                    ":"+ pacote.getPort() + "com tamanho:"+pacote.getLength());
+            System.out.write(pacote.getData(),0,pacote.getLength());
+            System.out.println("");
+            s.leaveGroup(InetAddress.getByName(ip));
+            s.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServidorMulticast.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+}
---
+++ bancosd/src/ServidorMulticast/ServidorSocket.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ServidorMulticast;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ivaldojunior
+ */
+public class ServidorSocket {
+
+    public static void main(String args[]) throws SocketException {
+
+        try {
+            int port = 2525;
+            DatagramSocket socket = new DatagramSocket(port);
+            byte[] buffer = new byte[1000];
+            System.out.println("Servidor aguardando request");
+            DatagramPacket r = new DatagramPacket(buffer, buffer.length);
+            socket.receive(r);
+            System.out.println("Request recebido de: "+ r.getAddress());
+            DatagramPacket resp = new DatagramPacket(r.getData(), r.getLength(), r.getAddress(), r.getPort());
+            socket.send(resp);
+            socket.close();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ServidorSocket.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
