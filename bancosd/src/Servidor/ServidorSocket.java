39a456a3e216d410dd801d09370a7542bfbaee67
ivaldojr <ivaldo.martins.jr@hotmail.com>
2015-05-28 17:37:04 -0300

    Migrando projeto que iniciou.

--- bancosd/src/Servidor/ServidorSocket.java
+++ bancosd/src/Servidor/ServidorSocket.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ServidorMulticast;
+package Servidor;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
